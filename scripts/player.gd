4dfd87a7e4426453ad1aa14553e8e4064600b12d
supajbro <supajbro@gmail.com>
2025-11-09 15:43:50 +1000

    Set functionality to switch weapons

--- scripts/player.gd
+++ scripts/player.gd
@@ -19,18 +19,17 @@ func _on_ready() -> void:
 	pistol_instance.position = global_position
 	add_child(pistol_instance)
 	
-	#machine_gun_instance = machine_gun_scene.instantiate()
-	#machine_gun_instance.position = global_position
-	#add_child(machine_gun_instance)
+	machine_gun_instance = machine_gun_scene.instantiate()
+	machine_gun_instance.position = global_position
+	add_child(machine_gun_instance)
 	
-	#shotgun_instance = shotgun_scene.instantiate()
-	#shotgun_instance.position = global_position
-	#add_child(shotgun_instance)
+	shotgun_instance = shotgun_scene.instantiate()
+	shotgun_instance.position = global_position
+	add_child(shotgun_instance)
 	
 	selected_weapon = pistol_instance
 
 func _physics_process(delta: float) -> void:
-
 	# Control movement
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed("MoveRight"):
@@ -45,6 +44,8 @@ func _physics_process(delta: float) -> void:
 	velocity = velocity.normalized() * speed
 	move_and_slide()
 	
+	# Handle gun controls
+	switch_weapon()
 	shoot(delta)
 	
 func shoot(delta: float):
@@ -53,3 +54,11 @@ func shoot(delta: float):
 			selected_weapon.shoot(global_position)
 		else:
 			push_error("Selected weapon is null")
+			
+func switch_weapon():
+	if Input.is_action_pressed("slot_one"):
+		selected_weapon = pistol_instance
+	elif Input.is_action_pressed("slot_two"):
+		selected_weapon = machine_gun_instance
+	elif Input.is_action_pressed("slot_three"):
+		selected_weapon = shotgun_instance
