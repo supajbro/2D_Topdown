5b02a7db2a88c2fe97cf4c1e0fae346ad3aae7f7
supajbro <supajbro@gmail.com>
2025-11-09 15:39:03 +1000

    Init setting up functionality to allow for multiple weapons

---
+++ scripts/gun.gd
@@ -0,0 +1,20 @@
+class_name gun
+extends Node
+
+@export var bullet_scene: PackedScene
+var bullet_instance: bullet = null
+
+var time_since_last_shot: float = 0
+@export var fire_rate: float = 0.5
+
+func _process(delta: float) -> void:
+	time_since_last_shot += delta
+
+func shoot(pos: Vector2):
+	if(time_since_last_shot < fire_rate):
+		return
+		
+	time_since_last_shot = 0
+	bullet_instance = bullet_scene.instantiate()
+	bullet_instance.position = pos
+	get_tree().current_scene.add_child(bullet_instance)
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,11 +2,32 @@ extends CharacterBody2D
 
 @export var speed: float = 50
 
-@export var bullet_scene: PackedScene
-var bullet_instance: bullet = null
+@export var pistol_scene: PackedScene
+var pistol_instance: gun = null
 
-var time_since_last_shot: float = 0
-@export var fire_rate: float = 0.5
+@export var machine_gun_scene: PackedScene
+var machine_gun_instance: gun = null
+
+@export var shotgun_scene: PackedScene
+var shotgun_instance: gun = null
+
+# the active weapon the player is using
+var selected_weapon: gun = null
+
+func _on_ready() -> void:
+	pistol_instance = pistol_scene.instantiate()
+	pistol_instance.position = global_position
+	add_child(pistol_instance)
+	
+	#machine_gun_instance = machine_gun_scene.instantiate()
+	#machine_gun_instance.position = global_position
+	#add_child(machine_gun_instance)
+	
+	#shotgun_instance = shotgun_scene.instantiate()
+	#shotgun_instance.position = global_position
+	#add_child(shotgun_instance)
+	
+	selected_weapon = pistol_instance
 
 func _physics_process(delta: float) -> void:
 
@@ -27,13 +48,8 @@ func _physics_process(delta: float) -> void:
 	shoot(delta)
 	
 func shoot(delta: float):
-	time_since_last_shot += delta
-	
-	if(time_since_last_shot < fire_rate):
-		return
-	
 	if Input.is_action_pressed("Shoot"):
-		time_since_last_shot = 0
-		bullet_instance = bullet_scene.instantiate()
-		bullet_instance.position = global_position
-		get_tree().current_scene.add_child(bullet_instance)
+		if selected_weapon:
+			selected_weapon.shoot(global_position)
+		else:
+			push_error("Selected weapon is null")
